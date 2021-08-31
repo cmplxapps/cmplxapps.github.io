b21a99c2c2362b8e1874945214e0c189cc12b008
Jim NoName <jimnoname@protonmail.com>
2021-08-31 14:57:05 -0700

    Create artcl.smedia.php

---
+++ artcl.smedia.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+	<body>
+		<main>
+			<article>
+				<section>
+					<h2>Social Media</h2><br/>
+				</section>
+				<section>
+					<p>
+						>>
+						<a href="https://www.patreon.com/jnnsoft" target="_blank">
+							Patreon</a></p>
+					<p>
+						>>
+						<a href="https://www.youtube.com/channel/UCPKq46z0ajiParWEiuNxybA" target="_blank">
+							YouTube</a></p>
+					<p>
+						>>
+						<a href="https://github.com/cmplxapps" target="_blank">
+							Github</a></p>
+					<p>
+						>>
+						<a href="https://twitter.com/___jjmx___" target="_blank">
+							Twitter (@___jjmx___)</a></p>
+				</section>
+			</article>
+		</main>
+	</body>
+</html>
