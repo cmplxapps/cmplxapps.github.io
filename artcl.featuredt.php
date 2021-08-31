c04de06853bd7a27bb3f839d21dedff4af676922
Jim NoName <jimnoname@protonmail.com>
2021-08-31 14:47:11 -0700

    Create artcl.featuredt.php

---
+++ artcl.featuredt.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+	<body>
+		<main>
+			<article>
+				<section>
+					<h2>Featured Software</h2><br/>
+				</section>
+				<section>
+					<p>
+						>>
+						<a href="https://cmplxapps.github.io/Swordfish">
+							Swordfish</a></p>
+					<p>
+						>>
+						<a href="https://cmplxapps.github.io/software/the_complex_calculator">
+							The Complex Calculator</a></p>
+					</section>
+					<section>
+					<p>
+						<a href="software">
+							See all my software >></a></p><br/>
+				</section>
+			</article>
+		</main>
+	</body>
+</html>
